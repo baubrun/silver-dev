b2563b054b3668141c872304d551443c22aeb046
baubrun <baubelf@gmail.com>
2021-07-21 19:23:38 -0400

    added markdown in postlist

--- web/src/components/PostList.js
+++ web/src/components/PostList.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { sorter } from '../helpers';
 import moment from 'moment';
+import ReactMarkdown from 'react-markdown';
 
 const PostList = (props) => {
   const [postItems, setPostItems] = useState([]);
-  const { items, onClick } = props;
+  const { items, onClick, selectedId } = props;
 
   const handleListItemClick = (id) => {
     onClick(id);
@@ -19,24 +20,32 @@ const PostList = (props) => {
     return <div>No Authors</div>;
   }
 
-  return postItems?.map((item) => (
-    <div key={item?.id}>
-      <div
-        className="title"
-        onClick={() => handleListItemClick(item.author?.id)}
-      >
-        <h2>Title: {`${item?.title}`}</h2>
+  const listItems = (item) => {
+    return (
+      <div key={item?.id}>
+        <div className="title" onClick={() => handleListItemClick(item?.id)}>
+          <h2>Title: {`${item?.title}`}</h2>
+        </div>
+        <ul>
+          <li>Summary: {`${item?.body.split('\n\n')[0]} ...`}</li>
+          <li>Author: {`${item.author?.name}`}</li>
+          <li>
+            published:{' '}
+            {`${moment(item?.publishedAt).format('DD MMMM YYYY, hh:mm a')}`}
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>Summary: {`${item?.body.split('\n\n')[0]} ...`}</li>
-        <li>Author: {`${item.author?.name}`}</li>
-        <li>
-          published:{' '}
-          {`${moment(item?.publishedAt).format('DD MMMM YYYY, hh:mm a')}`}
-        </li>
-      </ul>
-    </div>
-  ));
+    );
+  };
+
+  return postItems?.map((item, idx) => {
+    if (selectedId === item?.id) {
+      return <ReactMarkdown key={idx} children={listItems(item)} />;
+      // return <div key={idx}>allo</div>;
+    } else {
+      return listItems(item);
+    }
+  });
 };
 
 export default PostList;
